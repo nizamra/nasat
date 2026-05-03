c28b6d8b8b3afea3f6a9f4163dc42d918bebd076
nizamra <nizamra_95@hotmail.com>
2026-05-03 16:51:53 +0300

    adding DB service and be initcontainers

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -92,3 +92,19 @@ DATABASES = {
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
